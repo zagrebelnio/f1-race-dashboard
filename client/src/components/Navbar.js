ee081f01632a1e4ff714b866ef3f907884927935
Oleksandr <zagrebelniysasha@gmail.com>
2024-09-09 21:44:12 +0300

    feat: add basic styling rd-2

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,68 @@
 import { NavLink } from "react-router-dom";
 
 import logo from "../assets/logo.svg";
+import classes from "./Navbar.module.css";
 
 function Navbar() {
   return (
-    <header>
-      <nav>
-        <img src={logo} alt="Logo" />
-        <h1>Race Dashboard</h1>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/drivers">Drivers</NavLink>
-        <NavLink to="/constructors">Constructors</NavLink>
-        <NavLink to="/standings">Standings</NavLink>
-        <NavLink to="/results">Results</NavLink>
-      </nav>
-    </header>
+    <>
+      <header className={classes.header}>
+        <nav className={classes.nav}>
+          <div className={classes.logo}>
+            <img src={logo} alt="Logo" />
+            <h1>Race Dashboard</h1>
+          </div>
+          <div className={classes.links}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/drivers"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Drivers
+            </NavLink>
+            <NavLink
+              to="/constructors"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Constructors
+            </NavLink>
+            <NavLink
+              to="/standings"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Standings
+            </NavLink>
+            <NavLink
+              to="/results"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Results
+            </NavLink>
+          </div>
+        </nav>
+      </header>
+      <h1>bla bla bla</h1>
+    </>
   );
 }
 
