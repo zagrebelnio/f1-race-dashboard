a87ef22ab1ab625226ad8241f7f6247c8d77bb08
Oleksandr <zagrebelniysasha@gmail.com>
2024-09-09 15:20:47 +0300

    feat: add dummy routes rd-1

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,35 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Root Layout</div>,
+    errorElement: <div>Error Page</div>,
+    children: [
+      {
+        index: true,
+        element: <div>Home Page</div>,
+      },
+      {
+        path: "drivers",
+        element: <div>Drivers Page</div>,
+      },
+      {
+        path: "constructors",
+        element: <div>Constructors Page</div>,
+      },
+      {
+        path: "standings",
+        element: <div>Standings Page</div>,
+      },
+      {
+        path: "results",
+        element: <div>Results Page</div>,
+      },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
