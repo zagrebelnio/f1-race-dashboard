2b6a70e321944756edefef05e2235dfe180f3065
Oleksandr <zagrebelniysasha@gmail.com>
2024-09-09 22:28:57 +0300

    feat: add footer rd-3

--- client/src/pages/Root.js
+++ client/src/pages/Root.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function RootLayout() {
   return (
@@ -8,6 +9,7 @@ function RootLayout() {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
