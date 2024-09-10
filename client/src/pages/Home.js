9ecba80b1102bdbf89a457691cbcc704d317a5b0
Oleksandr <zagrebelniysasha@gmail.com>
2024-09-10 12:12:23 +0300

    feat: add home page rd-7

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import classes from "./Home.module.css";
+
+function HomePage() {
+  return (
+    <>
+      <section className={classes.title}>
+        <div>
+          <h1>Explore Formula 1 Championship with our Race Dashboard</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default HomePage;
