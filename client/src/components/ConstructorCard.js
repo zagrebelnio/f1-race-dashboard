b44c23832e11eefd7ae8823e8001f8fe3d5f5117
Oleksandr <zagrebelniysasha@gmail.com>
2024-10-30 12:29:33 +0200

    feat: add constructors api route

--- client/src/components/ConstructorCard.js
+++ client/src/components/ConstructorCard.js
@@ -1,10 +1,11 @@
-import classes from "./ConstructorCard.module.css";
+import logo from '../assets/default-team.jpg';
+import classes from './ConstructorCard.module.css';
 
 function ConstructorCard({ team }) {
   return (
     <div className={classes.card} style={{ borderColor: team.color }}>
       <div className={classes.info}>
-        <img className={classes.logo} src={team.logo} alt={team.name} />
+        <img className={classes.logo} src={team.logo ?? logo} alt={team.name} />
       </div>
       <div className={classes.details}>
         <p>{team.name}</p>
