c99e6bfe82b8b2a0633d727d2f53dcd6f4a5a5ee
Oleksandr <zagrebelniysasha@gmail.com>
2024-11-05 00:17:38 +0200

    feat: add results/qualifying route

--- server/api/results.js
+++ server/api/results.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import raceRoute from './results/race.js';
+import qualifyingRoute from './results/qualifying.js';
 
 const router = express.Router();
 
 router.use('/race', raceRoute);
+router.use('/qualifying', qualifyingRoute);
 
 export default router;
