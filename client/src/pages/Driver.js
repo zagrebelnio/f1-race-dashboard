303f39c1a2dc161354d67c72d993544c262090ff
Oleksandr <zagrebelniysasha@gmail.com>
2024-11-14 01:22:49 +0200

    feat: add profile section

--- client/src/pages/Driver.js
+++ client/src/pages/Driver.js
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import classes from './Driver.module.css';
 import { useEffect, useState } from 'react';
+import defaultDriverImage from '../assets/default-driver.png';
 
 function DriverPage() {
   const { id } = useParams();
@@ -23,6 +24,30 @@ function DriverPage() {
 
   return (
     <>
+      <section className={classes.profile}>
+        <img
+          className={classes.profilePic}
+          src={driverData.image ?? defaultDriverImage}
+          alt="Driver profile"
+        />
+        <div className={classes.info}>
+          <h1>{driverData.name}</h1>
+          <div className={classes.nationality}>
+            <img
+              className={classes.flag}
+              src={`https://flagsapi.com/${driverData.countryCode}/flat/64.png`}
+              alt="Flag"
+            />
+            <p>{driverData.nationality}</p>
+          </div>
+          <p>{driverData.abbreviation}</p>
+          <p>{driverData.number ?? 'N/A'}</p>
+          <p>
+            {new Date(driverData.dateOfBirth).toLocaleDateString('en-GB')} -{' '}
+            {driverData.dateOfDeath ?? 'present'}
+          </p>
+        </div>
+      </section>
       <h1>Driver Page for Driver {id}</h1>
       <div className={classes.driverData}>
         {Object.entries(driverData).map(([key, value]) => (
