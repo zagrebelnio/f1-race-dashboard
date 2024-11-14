7dfb11017dd3997ed0d0bb2122c54a6c8a40ff38
Oleksandr <zagrebelniysasha@gmail.com>
2024-11-14 02:23:13 +0200

    feat: add links to driver page

--- client/src/components/DriverCard.js
+++ client/src/components/DriverCard.js
@@ -1,42 +1,45 @@
 import classes from './DriverCard.module.css';
 import logo from '../assets/logo.svg';
 import defaultDriverImage from '../assets/default-driver.png';
+import { NavLink } from 'react-router-dom';
 
 function DriverCard({ driver }) {
   return (
-    <div
-      id={driver.id}
-      className={classes.card}
-      style={{ borderColor: driver.team.color }}
-    >
-      <div className={classes.profile}>
-        <div className={classes.info}>
-          <p className={classes.abbreviation}>{driver.abbr}</p>
+    <NavLink to={`/drivers/${driver.id}`}>
+      <div
+        id={driver.id}
+        className={classes.card}
+        style={{ borderColor: driver.team.color }}
+      >
+        <div className={classes.profile}>
+          <div className={classes.info}>
+            <p className={classes.abbreviation}>{driver.abbr}</p>
+            <img
+              className={classes.logo}
+              src={driver.team.logo ?? logo}
+              alt="Team logo"
+            />
+            <p className={classes.number}>{driver.number ?? 'N/A'}</p>
+            <img
+              className={classes.flag}
+              src={`https://flagsapi.com/${driver.countryCode}/flat/64.png`}
+              alt="Driver flag"
+            />
+          </div>
           <img
-            className={classes.logo}
-            src={driver.team.logo ?? logo}
-            alt="Team logo"
-          />
-          <p className={classes.number}>{driver.number ?? 'N/A'}</p>
-          <img
-            className={classes.flag}
-            src={`https://flagsapi.com/${driver.countryCode}/flat/64.png`}
-            alt="Driver flag"
+            className={classes.img}
+            src={driver.image ?? defaultDriverImage}
+            alt="Driver pic"
           />
         </div>
-        <img
-          className={classes.img}
-          src={driver.image ?? defaultDriverImage}
-          alt="Driver pic"
-        />
+        <div className={classes.details}>
+          <p>
+            {driver.firstName} {driver.lastName}
+          </p>
+          <p>{driver.team.name}</p>
+        </div>
       </div>
-      <div className={classes.details}>
-        <p>
-          {driver.firstName} {driver.lastName}
-        </p>
-        <p>{driver.team.name}</p>
-      </div>
-    </div>
+    </NavLink>
   );
 }
 
