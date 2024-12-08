1433bcfd44cb1a588acb0c6f1cd0357ae2c7b02d
Viktoriia <planshetassus@gmail.com>
2024-12-09 00:07:20 +0100

    add classes for center text in the table

--- client/src/components/ResultsTable.js
+++ client/src/components/ResultsTable.js
@@ -1,10 +1,12 @@
 import classes from './ResultsTable.module.css'
+import fastestLap from '../assets/fastestLap.png'
 
 const ResultsTable = ({ activeButton }) => {
+
   return (
       <section className={classes.tableBody}>
-        <table>
-          <thead>
+        <table className={classes.table}>
+          <thead className={classes.thead}>
               <tr>
                   <th>Pos</th>
                   <th>Driver</th>
@@ -14,22 +16,22 @@ const ResultsTable = ({ activeButton }) => {
                   <th>Points</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className={classes.tbody}>
               <tr>
-                  <td className='center-text-table'>1</td>
+                  <td className={classes.centerTextTable}>1</td>
                   <td>Max VERSTAPPEN</td>
                   <td className={classes.constructorColumn}>Red Bull Racing</td>
                   <td>1:14:40.727</td>
-                  <td className='center-text-table'>52</td>
-                  <td className='center-text-table'>25</td>
+                  <td className={classes.centerTextTable}>52</td>
+                  <td className={classes.centerTextTable}>25</td>
               </tr>
               <tr>
-                  <td className='center-text-table'>2</td>
+                  <td className={classes.centerTextTable}>2</td>
                   <td>Lando NORRIS</td>
                   <td className={classes.constructorColumn}>McLaren</td>
-                  <td>+2.44</td>
-                  <td className='center-text-table'>52</td>
-                  <td className='center-text-table'>18</td>
+                  <td className={classes.fastestLap}><span className={classes.span}>+2.44</span><img src={fastestLap} alt='fastest lap clock'/></td>
+                  <td className={classes.centerTextTable}>52</td>
+                  <td className={classes.centerTextTable}>18</td>
               </tr>
             </tbody>
           </table>  
