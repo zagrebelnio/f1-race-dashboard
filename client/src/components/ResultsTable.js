68e505dee3984c5daa82e143c575790ccd07cf06
Viktoriia <planshetassus@gmail.com>
2024-12-08 23:23:06 +0100

    center text

--- client/src/components/ResultsTable.js
+++ client/src/components/ResultsTable.js
@@ -16,20 +16,20 @@ const ResultsTable = ({ activeButton }) => {
             </thead>
             <tbody>
               <tr>
-                  <td>1</td>
+                  <td className='center-text-table'>1</td>
                   <td>Max VERSTAPPEN</td>
                   <td className={classes.constructorColumn}>Red Bull Racing</td>
                   <td>1:14:40.727</td>
-                  <td>52</td>
-                  <td>25</td>
+                  <td className='center-text-table'>52</td>
+                  <td className='center-text-table'>25</td>
               </tr>
               <tr>
-                  <td>2</td>
+                  <td className='center-text-table'>2</td>
                   <td>Lando NORRIS</td>
                   <td className={classes.constructorColumn}>McLaren</td>
                   <td>+2.44</td>
-                  <td>52</td>
-                  <td>18</td>
+                  <td className='center-text-table'>52</td>
+                  <td className='center-text-table'>18</td>
               </tr>
             </tbody>
           </table>  
